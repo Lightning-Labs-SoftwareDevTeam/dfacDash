eca1a3d3071c9307cf0024909a3e041f37d14bb1
Crissy Cabrera <crissymichelle@proton.me>
2024-01-24 11:58:28 -1000

    added PATCH route for dfac supervisors

--- routes/dfacs.js
+++ routes/dfacs.js
@@ -7,16 +7,16 @@ const router = express.Router();
 
 const jsonschema = require("jsonschema");
 const newDfacSchema = require("../schemas/dfacNew.json");
+const dfacUpdateSchema = require("../schemas/dfacUpdate.json");
 const Dfac = require("../models/dfac");
 
 const { ensureLoggedIn, ensureAdmin, authenticateJWT } = require("../middleware/auth");
 const { BadRequestError } = require("../expressError");
-const { createToken } = require("../helpers/tokens");
-const { json } = require("body-parser");
 
 /** CRUD router functions for DFAC data */
 
 /** POST route for creating a new DFAC
+ *  -- CREATE --
  * 
  * Adds a new dfac to the database, but only for admin users. Must include
  *  {dfacName, street, city, state, zip, dfacPhone} as input, 
@@ -39,6 +39,47 @@ router.post("/", authenticateJWT, ensureLoggedIn, ensureAdmin, async(req, res, n
     }
 });
 
-/** GET routes for reading dfac data is in the auth.js routes file. No authorization required */
+/** ~~~~~
+ * GET routes for reading dfac data is in the auth.js routes file. No authorization required
+ * ~~~~~
+ */
+
+/** PATCH route for modifying dfac data
+ *          -UPDATE-
+ * 
+ * data can include { dfacName, dfacLogo, street, bldgNum, city, state, zip, dfacPhone,flashMsg1, flashMsg2,
+ *                      bfHours, luHours, dnHours, bchHours, supHours, orderBf, orderLu,
+ *                      orderDn, orderBch, orderSup }
+ * 
+ * returns
+ *      { dfac: {dfacID, dfacName, dfacLogo, street, bldgNum, city, state, zip, dfacPhone, flashMsg1, flashMsg2,
+ *                       bfHours, luHours, dnHours, bchHours, supHours, orderBf, orderLu, orderDn, orderBch, orderSup} }
+ * 
+ * Requires supervisory rights
+ */
+router.patch("/:dfacID", authenticateJWT, ensureLoggedIn, async (req, res, next) => {
+    try {
+        const dfacTarget = req.params.dfacID;
+        const requestorDfacID = res.user.dfacID;
+
+        const validator = jsonschema.validate(req.body, dfacUpdateSchema);
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
+
+        if (res.locals.user.isAdmin) {
+            const dfac = await Dfac.update(dfacTarget, req.body);
+            return res.json({ dfac });
+        } else if (requestorDfacID === dfacTarget && res.locals.user.isManager) {
+            const dfac = await Dfac.update(dfacTarget, req.body);
+            return res.json({ dfac });
+        } else {
+            throw new ForbiddenError("Access denied");
+        }
+    } catch (err) {
+        return next(err);
+    }
+});
 
 module.exports = router;
