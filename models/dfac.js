baaa4e4b31766a6dc522fe29284f99d4854ca65e
Crissy Cabrera <crissymichelle@proton.me>
2024-01-24 12:17:09 -1000

    added PATCH route for dfac hours only

--- models/dfac.js
+++ models/dfac.js
@@ -215,6 +215,68 @@ class Dfac {
 
         return dfac;
     }
+
+    /** PATCH function to update dfac hours with `data` 
+     *          - UPDATE - 
+     * 
+     * Partial update is perfectly acceptable; fields only changed if patch request
+     * includes corresponding data.
+     * 
+     * Allowable data:
+     *      {bfHours, luHours, dnHours, bchHours, supHours, orderBf, orderLu, orderDn, orderBch, orderSup}
+     * 
+     * returns
+     * { dfac: {dfacID, dfacName, dfacLogo, street, bldgNum, city, state, zip, dfacPhone, flashMsg1, flashMsg2,
+     *                  bfHours, luHours, dnHours, bchHours, supHours, orderBf, orderLu, orderDn, orderBch, orderSup} }
+     *
+     * throws NotFoundError if dfacID not found
+     */
+    static async updateHours(dfacID, data) {
+        const { setCols, values } = sqlForPartialUpdate(
+            data,
+            {
+                bfHours: "bf_hours",
+                luHours: "lu_hours",
+                dnHours: "dn_hours",
+                bchHours: "bch_hours",
+                supHours: "sup_hours",
+                orderBf: "order_timebf",
+                orderLu: "order_timelu",
+                orderDn: "order_timedn",
+                orderBch:"order_timebch",
+                orderSup: "order_timesup"      
+            }
+        );
+        const dfacIDVarIdx = "$" + (values.length + 1);
+
+        const querySql = `UPDATE customers
+                            SET ${setCols}
+                            WHERE id = ${dfacIDVarIdx}
+                            RETURNING id AS "dfacID",
+                                    dfac_name AS "dfacName", 
+                                    dfac_logo AS "dfacLogo", 
+                                    street_address AS "street", 
+                                    bldg_num AS bldgNum,
+                                    city, 
+                                    state_abb AS "state", 
+                                    zip_code AS "zip", 
+                                    dfac_phnumber AS "dfacPhone", 
+                                    flash_msg1 AS "flashMsg1",
+                                    flash_msg2 AS "flashMsg2", 
+                                    bf_hours AS "bfHours", 
+                                    lu_hours AS "luHours", 
+                                    dn_hours AS "dnHours", 
+                                    order_timebf AS "orderBf", 
+                                    order_timelu AS "orderLu", 
+                                    order_timedn AS "orderDn", 
+                                    order_timesup AS "orderSup"`;
+        const result = await db.query(querySql, [...values, dfacID]);
+
+        const dfac = result.rows[0];
+        if (!dfac) throw new NotFoundError(`No dfac: ${dfacID}`);
+
+        return dfac;
+    }
 }
 
 module.exports = Dfac;
